9f803ffd5246f37111156d56c0c797286057837c
Robert Lieblang <roberttheanimal01@gmail.com>
2023-10-10 19:14:07 -0500

    Change Prisma Client Utility

--- src/middleware/checkUserExistence.ts
+++ src/middleware/checkUserExistence.ts
@@ -1,5 +1,5 @@
 import { NextFunction, Request, Response } from "express";
-import prisma from "../utils/prismaClientUtil";
+import prisma from "../utils/prisma";
 
 export const checkUserExistence = async (
   req: Request,
